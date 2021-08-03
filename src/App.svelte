5678a30081eff505cdcb98da6f0ddadb2a7d34d6
Rajk1012 <rajkumardhakar387@gmail.com>
2021-08-03 17:53:17 +0530

    tempalets running

--- src/App.svelte
+++ src/App.svelte
@@ -24,7 +24,7 @@ import axios from  "axios"
       <mb-context path="encounter_1/age/language"></mb-context>
       <mb-context path="encounter_1/age/encoding"></mb-context>
       <mb-context path="encounter_1/age/subject"></mb-context>
-      
+
     <mb-date path="birthdate" label="Date of Birth"></mb-date>
     <div  >
       <mb-buttons type="code" label="Gender" path="gender">
@@ -62,7 +62,7 @@ import axios from  "axios"
       <mb-option label="RTPCR" value="RTPCR"></mb-option>
       <mb-option label="None" value="none"></mb-option>
     </mb-buttons></div>
-    <mb-buttons path="symptom" type="code" label="Symptoms/Signs" multiple>
+    <mb-select path="symptom" type="code" label="Symptoms/Signs" multiple>
       <mb-option value="fever" label="Fever"></mb-option>
       <mb-option value="fatigue" label="Fatigue"></mb-option>
       <mb-option value="drycough" label="Dry cough"></mb-option>
@@ -72,7 +72,7 @@ import axios from  "axios"
       <mb-option value="musclepain" label="Muscle Pain"></mb-option>
       <mb-option value="shrtnessO2" label="Shortness of breath"></mb-option>
       <mb-option value="diarrhea" label="Diarrhea"></mb-option>
-    </mb-buttons>
+    </mb-select>
     <mb-input label="Medication" path="medication" ></mb-input>
     <mb-input label="Cormobodities" path="cormobodities" ></mb-input>
     <mb-input label="SpO2 Level" path="SpO2 level" ></mb-input>
@@ -84,39 +84,46 @@ import axios from  "axios"
       <mb-option value="none" label="None"></mb-option>
     </mb-buttons></div>
      -->
-<mb-input path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/screening_purpose" label="Screening purpose"></mb-input>
+<mb-select path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/screening_purpose" label="Screening purpose">
+  <mb-option value="SDC-SARS" label="Suspected disease cause by SARS-CoV-2"></mb-option>
+</mb-select>
 <mb-select path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/presence_of_any_symptoms_or_signs" label="Presence of any symptoms or signs?">
-            <mb-option code="at0031" display="Present"></mb-option>
-<mb-option code="at0032" display="Absent"></mb-option>
-<mb-option code="at0033" display="Unknown"></mb-option>
+            <mb-option value="at0023" label="Present"></mb-option>
+            <mb-option value="at0024" label="Absent"></mb-option>
+            <mb-option value="at0027" label="Unknown"></mb-option>
           </mb-select>
 <mb-date time path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/onset_of_any_symptoms_or_signs" label="Onset of any symptoms or signs"></mb-date>
 <mb-input path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/description" label="Description"></mb-input>
-<mb-input path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/specific_symptom_sign:0/symptom_or_sign_name" label="Symptom or sign name"></mb-input>
-<mb-select path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/specific_symptom_sign:0/presence" label="Presence?">
-            <mb-option code="at0023" display="Present"></mb-option>
-<mb-option code="at0024" display="Absent"></mb-option>
-<mb-option code="at0027" display="Unknown"></mb-option>
-          </mb-select>
+<mb-select path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/specific_symptom_sign:0/symptom_or_sign_name" label="Symptom or sign name">
+    <mb-option value="fever" label="Fever"></mb-option>
+    <mb-option value="fatigue" label="Fatigue"></mb-option>
+    <mb-option value="drycough" label="Dry cough"></mb-option>
+    <mb-option value="nasalCong" label="Nasal congestion"></mb-option>
+    <mb-option value="nasaldischarge" label="Nasal Discharge"></mb-option>
+    <mb-option value="sorethroat" label="Sore Throat"></mb-option>
+    <mb-option value="musclepain" label="Muscle Pain"></mb-option>
+    <mb-option value="shrtnessO2" label="Shortness of breath"></mb-option>
+    <mb-option value="diarrhea" label="Diarrhea"></mb-option>
+  </mb-select>
+      <mb-select path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/specific_symptom_sign:0/presence" label="Presence?">
+          <mb-option value="at0023" label="Present"></mb-option>
+          <mb-option value="at0024" label="Absent"></mb-option>
+          <mb-option value="at0027" label="Unknown"></mb-option>
+      </mb-select>
 <mb-input path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/specific_symptom_sign:0/comment" label="Comment"></mb-input>
 <mb-input path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/overall_comment" label="Overall comment"></mb-input>
-<mb-context path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/time"></mb-context>
-<mb-context path="encounter_1/symptom_sign_screening_questionnaire/language"></mb-context>
-<mb-context path="encounter_1/symptom_sign_screening_questionnaire/encoding"></mb-context>
-<mb-context path="encounter_1/symptom_sign_screening_questionnaire/subject"></mb-context>
-<mb-input path="encounter_1/condition_screening_questionnaire/any_event:0/screening_purpose" label="Screening purpose"></mb-input>
-<mb-select path="encounter_1/condition_screening_questionnaire/any_event:0/presence_of_any_conditions" label="Presence of any conditions?">
-            <mb-option code="at0031" display="Present"></mb-option>
-<mb-option code="at0032" display="Absent"></mb-option>
-<mb-option code="at0033" display="Unknown"></mb-option>
-          </mb-select>
-<mb-input path="encounter_1/condition_screening_questionnaire/any_event:0/specific_condition:0/condition_name" label="Condition name"></mb-input>
-<mb-select path="encounter_1/condition_screening_questionnaire/any_event:0/specific_condition:0/presence" label="Presence?">
-            <mb-option code="at0023" display="Present"></mb-option>
-<mb-option code="at0024" display="Absent"></mb-option>
-<mb-option code="at0027" display="Unknown"></mb-option>
-          </mb-select>
+
 <mb-input path="encounter_1/condition_screening_questionnaire/any_event:0/comment" label="Comment"></mb-input>
+<mb-input label="Medication" path="medication" ></mb-input>
+          <mb-input label="Cormobodities" path="cormobodities" ></mb-input>
+          <mb-input label="SpO2 Level" path="SpO2 level" ></mb-input>
+          <mb-input label="Allergic To" path="allergy"></mb-input>         
+          <div >
+          <mb-buttons label="Immunization" type="code" path="Immunization">
+            <mb-option value="dose1" label="Dose-1"></mb-option>
+            <mb-option value="dose2" label="Dose-2"></mb-option>
+            <mb-option value="none" label="None"></mb-option>
+          </mb-buttons></div>
 <mb-context path="encounter_1/condition_screening_questionnaire/any_event:0/time"></mb-context>
 <mb-context path="encounter_1/condition_screening_questionnaire/language"></mb-context>
 <mb-context path="encounter_1/condition_screening_questionnaire/encoding"></mb-context>
