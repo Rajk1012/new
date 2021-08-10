b29f782890218521cb847a596797fe0f6bf67b23
Rajk1012 <rajkumardhakar387@gmail.com>
2021-08-10 21:21:51 +0530

    change

--- src/form.svelte
+++ src/form.svelte
@@ -15,6 +15,27 @@ import axios from  "axios"
   <h1 class="text-2xl font-semibold font-sans">Patient Registration Form</h1>
   <div >
   <mb-form class="flex flex-col gap-5" on:mb-submit={Click}>
+    <mb-context path="resourceType" data="patient"></mb-context>
+    <mb-input path="name[0].given" label="Name"></mb-input>
+
+   <mb-input path="encounter_1/age/age_category" label="Age"></mb-input>
+    <mb-date path="birthdate" label="Date of Birth"></mb-date>
+    <div  >
+      <mb-buttons type="value" label="Gender" path="gender">
+      <mb-option label="Male" value="male"></mb-option>
+      <mb-option label="Female" value="female"></mb-option>
+      <mb-option label="Other" value="other"></mb-option>
+    </mb-buttons>
+    </div>
+    <mb-input label="Aadhar No." path="identifier[0].value"></mb-input>
+    <mb-input path="telecome[0].value" label="Phone No."></mb-input>
+    <mb-input Label="Address" textarea path="address[0].text"></mb-input>
+    <div >
+    <mb-buttons type="value" label="Covid Test" path="covid test">
+      <mb-option label="Antigen" value="antigen"></mb-option>
+      <mb-option label="RTPCR" value="RTPCR"></mb-option>
+      <mb-option label="None" value="none"></mb-option>
+    </mb-buttons></div>
     <mb-context path="covid.form.v1/context/start_time"></mb-context>
 <mb-context path="covid.form.v1/context/setting"></mb-context>
 <mb-select path="covid.form.v1/clinical_background/covid_19_admission/hospital_admission_status" label="Hospital admission status">
