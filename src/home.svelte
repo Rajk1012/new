f8fe954688c4ad5e1d600887998dfea5611f9359
Rajk1012 <86977745+Rajk1012@users.noreply.github.com>
2021-08-10 14:37:57 +0530

    Add files via upload

---
+++ src/home.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import '@shoelace-style/shoelace/dist/themes/base.css'
+  import 'medblocks-ui'
+  import './tailwind.css'
+  import axios from  "axios"
+  import form from './form.svelte'
+  import { Router, Link, Route } from "svelte-routing"
+  import home from './home.svelte'  
+  </script>
+
+<div>
+        <div class="container">
+        <Link to="/form" class="link">New Patient</Link>
+        </div>
+        <div class="container">
+        <Link to="/Patient" class="link">Get Patient Info</Link>
+        </div>
+        <div class="container">
+        <Link to="/post" class="link">Post Covid Care</Link>
+        </div>
+</div>
