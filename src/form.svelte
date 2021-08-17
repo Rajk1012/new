4651f645c73acffbc412ac115c259dd7011bd12d
Rajk1012 <rajkumardhakar387@gmail.com>
2021-08-17 14:41:24 +0530

    "var adedd"

--- src/form.svelte
+++ src/form.svelte
@@ -1,7 +1,7 @@
 
 
 <script lang="ts">
-  import '@shoelace-style/shoelace/dist/themes/base.css'
+import '@shoelace-style/shoelace/dist/themes/base.css'
 import 'medblocks-ui'
 import './tailwind.css'
 import axios from  "axios"
@@ -10,6 +10,15 @@ import axios from  "axios"
   console.log(e.target.data)
   
 }
+// var form = document.querySelector('mb-form')
+//       form.cdr = axios.create({baseURL: "http://localhost:8080/ehrbase/rest/eci/v1"})
+//      form.template = "Covid.form.v1"
+//       form.ehr = "5338b7cf-947c-4a31-bd51-57e3e8d8e237"
+
+//     form.addEventListener('submit',(e)=>{
+//       form.post(e.detail).then(r=>alert(JSON.stringify(r)))
+//     })
+
 </script>
 <div class="container mx-auto sm:gx-8 max-w-5xl py-10">
   <h1 class="text-2xl font-semibold font-sans">Patient Registration Form</h1>
@@ -314,201 +323,6 @@ import axios from  "axios"
 <mb-context path="covid.form.v1/language"></mb-context>
 <mb-context path="covid.form.v1/territory"></mb-context>
 <mb-context path="covid.form.v1/composer"></mb-context>
-    <!-- <mb-context path="lab_results/context/start_time"></mb-context>
-<mb-context path="lab_results/context/setting"></mb-context>
-<mb-input path="lab_results/laboratory_test_result/any_event:0/test_name" label="Test name"></mb-input>
-<mb-select path="lab_results/laboratory_test_result/any_event:0/overall_test_status:0" label="Overall test status">
-      <mb-option value="at0107" label="Registered"></mb-option>
-      <mb-option value="at0037" label="Partial"></mb-option>
-      <mb-option value="at0120" label="Preliminary"></mb-option>
-      <mb-option value="at0038" label="Final"></mb-option>
-      <mb-option value="at0040" label="Amended"></mb-option>
-      <mb-option value="at0115" label="Corrected"></mb-option>
-      <mb-option value="at0119" label="Appended"></mb-option>
-      <mb-option value="at0074" label="Cancelled"></mb-option>
-      <mb-option value="at0116" label="Entered in error"></mb-option>
-</mb-select>
-<mb-date time path="lab_results/laboratory_test_result/any_event:0/overall_test_status_timestamp" label="Overall test status timestamp"></mb-date>
-<mb-input path="lab_results/laboratory_test_result/any_event:0/diagnostic_service_category" label="Diagnostic service category"></mb-input>
-<mb-input path="lab_results/laboratory_test_result/any_event:0/clinical_information_provided" label="Clinical information provided"></mb-input>
-<mb-input path="lab_results/laboratory_test_result/any_event:0/conclusion" label="Conclusion"></mb-input>
-<mb-input path="lab_results/laboratory_test_result/any_event:0/test_diagnosis:0" label="Test diagnosis"></mb-input>
-<mb-input path="lab_results/laboratory_test_result/any_event:0/comment:0" label="Comment"></mb-input>
-<mb-input path="lab_results/laboratory_test_result/any_event:0/confounding_factors:0" label="Confounding factors"></mb-input>
-<mb-context path="lab_results/laboratory_test_result/any_event:0/time"></mb-context>
-
-<mb-input path="lab_results/laboratory_test_result/laboratory_internal_identifier/text_value" label=""></mb-input>
-<mb-input path="lab_results/laboratory_test_result/test_request_details:0/original_test_requested_name:0" label="Original test requested name"></mb-input>
-
-<mb-input path="lab_results/laboratory_test_result/test_request_details:0/requester_order_identifier/text_value" label=""></mb-input>
-
-<mb-input path="lab_results/laboratory_test_result/test_request_details:0/receiver_order_identifier/text_value" label=""></mb-input>
-<mb-checkbox path="lab_results/laboratory_test_result/point-of-care_test" label="Point-of-care test"></mb-checkbox>
-<mb-context path="lab_results/laboratory_test_result/test_method/valued_text_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/text_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/multimedia_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/parsable_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/state_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/boolean_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/identifier_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/uri_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/ehr_uri_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/duration_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/quantity_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/count_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/proportion_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/date_time_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/date_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/time_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/ordinal_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/test_method/scale_value"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/language"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/encoding"></mb-context>
-<mb-context path="lab_results/laboratory_test_result/subject"></mb-context>
-
-<mb-quantity default="Cel" path="lab_results/body_temperature/any_event:0/temperature" label="Temperature">
-      <mb-unit unit="Cel" label="Cel"></mb-unit>
-      <mb-unit unit="[degF]" label="[degF]"></mb-unit>
-</mb-quantity>
-
-
-<mb-select path="lab_results/body_temperature/any_event:0/body_exposure" label="Body exposure">
-      <mb-option value="at0031" label="Naked"></mb-option>
-      <mb-option value="at0032"  label="Reduced clothing/bedding"></mb-option>
-      <mb-option value="at0033"  label="Appropriate clothing/bedding"></mb-option>
-      <mb-option value ="at0034"  label="Increased clothing/bedding"></mb-option>
-</mb-select>
-<mb-input path="lab_results/body_temperature/any_event:0/description_of_thermal_stress" label="Description of thermal stress"></mb-input>
-
-<mb-input path="lab_results/body_temperature/any_event:0/confounding_factors:0" label="Confounding factors"></mb-input>
-<mb-context path="lab_results/body_temperature/any_event:0/time"></mb-context>
-
-<mb-select path="lab_results/body_temperature/location_of_measurement" label="Location of measurement">
-      <mb-option value="at0025" label="Rectum"></mb-option>
-      <mb-option value="at0024" label="Axilla"></mb-option>
-      <mb-option value="at0023" label="Ear canal"></mb-option>
-      <mb-option value="at0061" label="Forehead"></mb-option>
-      <mb-option value="at0022" label="Mouth"></mb-option>
-      <mb-option value="at0026" label="Nasopharynx"></mb-option>
-      <mb-option value="at0027" label="Urinary bladder"></mb-option>
-      <mb-option value="at0028" label="Intravascular"></mb-option>
-      <mb-option value="at0043" label="Skin"></mb-option>
-      <mb-option value="at0051" label="Vagina"></mb-option>
-      <mb-option value="at0054" label="Oesophagus"></mb-option>
-      <mb-option value="at0055" label="Inguinal skin crease"></mb-option>
-      <mb-option value="at0060" label="Temple"></mb-option>
-</mb-select>
-
-<mb-context path="lab_results/body_temperature/language"></mb-context>
-<mb-context path="lab_results/body_temperature/encoding"></mb-context>
-<mb-context path="lab_results/body_temperature/subject"></mb-context>
-
-<mb-quantity default="ml/dl" path="lab_results/pulse_oximetry/any_event:0/spoc" label="SpOC">
-      <mb-unit unit="ml/dl" label="ml/dl"></mb-unit>
-</mb-quantity>
-
-
-<mb-input path="lab_results/pulse_oximetry/any_event:0/interpretation:0" label="Interpretation"></mb-input>
-<mb-input path="lab_results/pulse_oximetry/any_event:0/comment" label="Comment"></mb-input>
-<mb-input path="lab_results/pulse_oximetry/any_event:0/confounding_factors:0" label="Confounding factors"></mb-input>
-<mb-context path="lab_results/pulse_oximetry/any_event:0/time"></mb-context>
-<mb-input path="lab_results/pulse_oximetry/sensor_site" label="Sensor site"></mb-input>
-
-<mb-select path="lab_results/pulse_oximetry/pre_post-ductal" label="Pre/post-ductal">
-    <mb-option value="at0062" label="Pre-ductal"></mb-option>
-    <mb-option value="at0063" label="Post-ductal"></mb-option>
-    <mb-option value="at0064" label="Indeterminate"></mb-option>
-</mb-select>
-
-<mb-context path="lab_results/pulse_oximetry/language"></mb-context>
-<mb-context path="lab_results/pulse_oximetry/encoding"></mb-context>
-<mb-context path="lab_results/pulse_oximetry/subject"></mb-context>
-<mb-context path="lab_results/category"></mb-context>
-<mb-context path="lab_results/language"></mb-context>
-<mb-context path="lab_results/territory"></mb-context>
-<mb-context path="lab_results/composer"></mb-context> -->
-    <!-- <mb-context path="encounter_1/context/start_time"></mb-context>
-    <mb-context path="encounter_1/context/setting"></mb-context>
-    <mb-context path="resourceType" data="patient"></mb-context>
-    <mb-input path="name[0].given" label="Name"></mb-input>
-
-   <mb-input path="encounter_1/age/age_category" label="Age"></mb-input>
-    <mb-date path="birthdate" label="Date of Birth"></mb-date>
-    <div  >
-      <mb-buttons type="value" label="Gender" path="gender">
-      <mb-option label="Male" value="male"></mb-option>
-      <mb-option label="Female" value="female"></mb-option>
-      <mb-option label="Other" value="other"></mb-option>
-    </mb-buttons>
-    </div>
-    <mb-input label="Aadhar No." path="identifier[0].value"></mb-input>
-    <mb-input path="telecome[0].value" label="Phone No."></mb-input>
-    <mb-input Label="Address" textarea path="address[0].text"></mb-input>
-    <div >
-    <mb-buttons type="value" label="Covid Test" path="covid test">
-      <mb-option label="Antigen" value="antigen"></mb-option>
-      <mb-option label="RTPCR" value="RTPCR"></mb-option>
-      <mb-option label="None" value="none"></mb-option>
-    </mb-buttons></div>
-<mb-select path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/screening_purpose" label="Screening purpose">
-  <mb-option value="SDC-SARS" label="Suspected disease cause by SARS-CoV-2"></mb-option>
-</mb-select>
-<mb-select path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/presence_of_any_symptoms_or_signs" label="Presence of any symptoms or signs?">
-            <mb-option value="at0023" label="Present"></mb-option>
-            <mb-option value="at0024" label="Absent"></mb-option>
-            <mb-option value="at0027" label="Unknown"></mb-option>
-          </mb-select>
-<mb-select path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/specific_symptom_sign:0/symptom_or_sign_name" label="Symptom or sign name">
-    <mb-option value="fever" label="Fever"></mb-option>
-    <mb-option value="fatigue" label="Fatigue"></mb-option>
-    <mb-option value="drycough" label="Dry cough"></mb-option>
-    <mb-option value="nasalCong" label="Nasal congestion"></mb-option>
-    <mb-option value="nasaldischarge" label="Nasal Discharge"></mb-option>
-    <mb-option value="sorethroat" label="Sore Throat"></mb-option>
-    <mb-option value="musclepain" label="Muscle Pain"></mb-option>
-    <mb-option value="shrtnessO2" label="Shortness of breath"></mb-option>
-    <mb-option value="diarrhea" label="Diarrhea"></mb-option></mb-select>
-    <mb-input path="encounter_1/symptom_sign_screening_questionnaire/any_event:0/description" label="Description"></mb-input>
-
-<mb-select path="encounter_1/condition_screening_questionnaire/any_event:0/specific_condition:0/condition_name" label="Condition name">
-  <mb-option value="Hypomexia" label="Hypomexia"></mb-option>
-    <mb-option value="Cyanosis" label="Cyanosis"></mb-option>
-    <mb-option value="AR distress syndrome" label="Acute respiratory distress syndrome"></mb-option>
-    <mb-option value="septic Shock" label="Septic shock"></mb-option>
-    <mb-option value="metabolic Acidosis" label="Metabolic acidosis"></mb-option>
-    <mb-option value="Blood Coagulation Disorder" label="Blood coagulation disorder"></mb-option>
-    <mb-option value="shock" label="Shock"></mb-option>
-    <mb-option value="OrganFailure" label="Multiple organ failure"></mb-option>
-    <mb-option value="pregnant" label="Pregnant"></mb-option>
-    <mb-option value="Comorbid" label="Comorbid conditions"></mb-option>
-    <mb-option value="Immunodef" label="Immunodeficiency"></mb-option>
-    <mb-option value="ARF" label="Acute respiratory failure"></mb-option>
-</mb-select>
-<mb-select path="encounter_1/condition_screening_questionnaire/any_event:0/specific_condition:0/presence" label="Presence?">
-  <mb-option value="at0023" label="Present"></mb-option>
-  <mb-option value="at0024" label="Absent"></mb-option>
-  <mb-option value="at0027" label="Unknown"></mb-option>
-          </mb-select>
-
-<mb-input path="encounter_1/condition_screening_questionnaire/any_event:0/comment" label="Comment"></mb-input>
-<mb-context path="encounter_1/condition_screening_questionnaire/any_event:0/time"></mb-context>
-<mb-input label="Medication" path="medication" ></mb-input>
-          <mb-input label="SpO2 Level" path="SpO2 level" ></mb-input>
-          <mb-input label="Allergic To" path="allergy"></mb-input>         
-          <div >
-          <mb-buttons label="Immunization" type="value" path="Immunization">
-            <mb-option value="dose1" label="Dose-1"></mb-option>
-            <mb-option value="dose2" label="Dose-2"></mb-option>
-            <mb-option value="none" label="None"></mb-option>
-          </mb-buttons></div>
-<mb-context path="encounter_1/condition_screening_questionnaire/any_event:0/time"></mb-context>
-<mb-context path="encounter_1/condition_screening_questionnaire/language"></mb-context>
-<mb-context path="encounter_1/condition_screening_questionnaire/encoding"></mb-context>
-<mb-context path="encounter_1/condition_screening_questionnaire/subject"></mb-context>
-<mb-context path="encounter_1/category"></mb-context>
-<mb-context path="encounter_1/language"></mb-context>
-<mb-context path="encounter_1/territory"></mb-context>
-<mb-context path="encounter_1/composer"></mb-context> -->
     <mb-submit>
        <sl-button type="info">Submit</sl-button>
      </mb-submit>
